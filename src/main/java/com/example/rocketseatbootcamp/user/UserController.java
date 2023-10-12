c995662c8d5ff392c5ade4a407f85cd0c5756fe0
gabrielcombalia <gcombalia@gmail.com>
2023-10-12 09:17:45 -0300

    read Usuario

--- src/main/java/com/example/rocketseatbootcamp/user/UserController.java
+++ src/main/java/com/example/rocketseatbootcamp/user/UserController.java
@@ -1,5 +1,6 @@
 package com.example.rocketseatbootcamp.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private IUserRepository userRepository;
+
 
     /**
      * String (texto)
@@ -33,8 +37,9 @@ public class UserController {
      */
 
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
+    public UserModel create(@RequestBody UserModel userModel) {
         System.out.println(userModel.getUsername());
-
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
